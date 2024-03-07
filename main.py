66bb399bb63cb7c9869d3ef18f65c9556cd4cade
SAIKIRANPATNANA <saikiranpatnana5143@gmail.com>
2024-03-07 17:48:52 +0530

    updated

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import streamlit as st
 import os
 import subprocess
-from PIL import Image
+import ultralytics
+ultralytics.checks()
+from ultralytics import YOLO
+import shutil
 
-detect_script_path = "yolov5/detect.py"
-weights_path = "best.pt"
-output_dir = "tested_images"
+def delete_folder(folder_path):
+    try:
+        shutil.rmtree(folder_path)
+    except:
+        return
+
+model_path = 'best.pt'
+output_dir = 'runs/detect/predict'
 
 st.set_page_config(page_title='Sign Language Recognition Project', layout='centered')
-
 st.title('Null Class Intership Project')
-st.title("SIGN LANGUAGE DETECTION")
+st.title("Sign Language Recognition Project")
 st.header('Trained & Developed by Sai Kiran Patnana')
+delete_folder('runs')
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
@@ -20,16 +28,12 @@ if uploaded_file:
     with open(image_path, "wb") as f:
         f.write(uploaded_file.getvalue())
 
-    detect_command = f"python {detect_script_path} --weights {weights_path} --img 640 --conf 0.4 --source {image_path}"
+    detect_command = f"yolo task=detect mode=predict model={model_path} conf=0.5 source='{image_path}' save_txt=true save_conf=true"
     subprocess.run(detect_command, shell=True)
     
     output_image_path = os.path.join(output_dir, "test_image.jpg")
     if os.path.exists(output_image_path):
-        detected_image = Image.open(output_image_path)
-        st.image(detected_image, caption="Sign Language Recognition Result", use_column_width=True)
+        st.image(output_image_path, caption="Sign Language Recognition Result", use_column_width=True)
     else:
         st.write("Error: Output image not found")
-
-    os.remove(image_path)
-
-# model size: 15 mb
+    
